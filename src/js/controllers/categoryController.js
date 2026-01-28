15658d0e01c5a703d31bb08af734eaa87d124048
TonyHermann <alejohzero@gmail.com>
2026-01-29 00:29:14 +0100

    chore: move dialogs to src/ui

--- src/js/controllers/categoryController.js
+++ src/js/controllers/categoryController.js
@@ -1,15 +1,14 @@
 import { fetchCategories } from "../services/categoryService";
-import { createCategoryModal } from "../ui/dialgos/CategoryDialogs";
+import { createCategoryModal } from "../../ui/dialgos/CategoryDialogs";
 import { categoryStore } from "../store/categoryStore";
 
-
 const categoryController = () => {
-    $("#admCat").addEventListener("click", async (e) => {
-        e.preventDefault();
-        await fetchCategories();
-        let categories = categoryStore.getState();
-        createCategoryModal(categories);
-    });
+  $("#admCat").addEventListener("click", async (e) => {
+    e.preventDefault();
+    await fetchCategories();
+    let categories = categoryStore.getState();
+    createCategoryModal(categories);
+  });
 };
 
 export { categoryController };
