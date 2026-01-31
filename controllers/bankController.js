dade184af9b72e347adc5888d20450a256bc9e0f
TonyHermann <alejoh.developer@gmail.com>
2026-01-31 13:07:30 +0100

    fix: remove unused code

--- controllers/bankController.js
+++
@@ -1,8 +0,0 @@
-// Implement bankController functionality
-exports.getBankInfo = (req, res) => {
-  // Logic to get bank info
-};
-
-exports.transferFunds = (req, res) => {
-  // Logic to transfer funds
-};
