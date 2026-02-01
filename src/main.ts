3a1684e150bd973045edb142557f520ce7013526
TonyHermann <alejoh.developer@gmail.com>
2026-02-01 01:17:36 +0100

    feat: migrate mains.js to .ts

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import "./css/index.css";
+import "./js/router.js";
+import { assistant } from "./js/components/Assistant.js";
+import { categoryController } from "./js/controllers/categoryController.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  run();
+});
+
+const initAssistant = (assistant) => {
+  assistant.render($("body"));
+  assistant.greet();
+};
+
+const run = async () => {
+  initAssistant(assistant);
+  categoryController();
+};
