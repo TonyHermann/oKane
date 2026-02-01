36be8af9cbe96ade12b588532d96f57b4fe7d3df
TonyHermann <alejoh.developer@gmail.com>
2026-02-01 01:26:29 +0100

    fix: update index.css import

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import "./css/index.css";
+import "./ui/styles/css/index.css";
 import "./js/router.js";
 import { assistant } from "./js/components/Assistant.js";
 import { categoryController } from "./js/controllers/categoryController.js";
