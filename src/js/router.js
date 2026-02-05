25f81620eb039755a277e4cebb1059959349f4a5
TonyHermann <alejoh.developer@gmail.com>
2026-02-05 21:06:59 +0100

    Fix code formatting in router.js

--- src/js/router.js
+++ src/js/router.js
@@ -1,27 +1,25 @@
-import { routes } from "./data/routes.js"
+import { routes } from "./data/routes.js";
 
 const route = (event) => {
-    event = event || window.event;
-    event.preventDefault();
-    window.history.pushState({}, "", event.target.href);
-    handleLocation();
-}
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  handleLocation();
+};
 
 const handleLocation = async () => {
-    const path = window.location.pathname;
-    const routeItm = routes[path] || routes[404];
-    const route = routeItm.url
-    const html = await fetch(route).then((data) => {
-        return data.text();
-    })
-    document.querySelector(".main-content").innerHTML = html;
-    if(routes[path]) {
-        routes[path].js()
-    }
-}
+  const path = window.location.pathname;
+  const routeItm = routes[path] || routes[404];
+  const route = routeItm.url;
+  const html = await fetch(route).then((data) => data.text());
+  document.querySelector(".main-content").innerHTML = html;
+  if (routes[path]) {
+    routes[path].js();
+  }
+};
 
 window.onpopstate = handleLocation;
 window.route = route;
 
-
 handleLocation();
+
