852ce0a1537866c91b14133c2b8e2437e3316a35
TonyHermann <alejoh.developer@gmail.com>
2026-03-11 23:39:52 +0100

    chore(ui): save old pages as backup while working

---
+++ src/ui/_pages/home/index.js
@@ -0,0 +1,7 @@
+// import { assistant } from "../../main.js";
+import { Assistant } from "../../components/Assistant";
+const assistant = new Assistant();
+
+export const runIndex = () => {
+  assistant.say("¡Este es el inicio de お金!");
+};
