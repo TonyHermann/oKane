06c107967f2bcacc8e73fdbde032295ab584b12b
TonyHermann <alejoh.developer@gmail.com>
2026-03-17 20:25:10 +0100

    feat(ui): create MovementsPage component on it's own route

---
+++ src/ui/pages/movements/MovementsPage.tsx
@@ -0,0 +1,10 @@
+export const MovementsPage = () => (
+  <div className="window" style={{ padding: "20px" }}>
+    <div className="title-bar">
+      <div className="title-bar-text">Movimientos</div>
+    </div>
+    <div className="window-body">
+      <p>Movements Page - en construcción 🚧</p>
+    </div>
+  </div>
+);
