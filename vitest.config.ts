e371faad967b2ae06edf9681510c3f7224b36787
TonyHermann <alejoh.developer@gmail.com>
2026-04-09 23:27:32 +0200

    feat(tests): add jest to vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: [],
+    setupFiles: ["src/setupTests.ts"],
     include: ["src/**/*.test.ts", "src/**/*.test.tsx"],
   },
 });
